226eaab7418f1765a54bb33fa6eb0cf77ee45ecb
Emilio Bensignor <liobensignor@gmail.com>
2024-08-17 01:09:10 -0300

    Scroll Down + Jerarquia de clases + Hidden/Show

--- content/js/main.js
+++ content/js/main.js
@@ -9,3 +9,17 @@ nobili.forEach((section, index) => {
     pinSpacing: false,
   });
 });
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
